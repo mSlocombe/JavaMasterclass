78ee8862c4af05d098ba76179d600537b36134d8
mSlocombe <slocombe90@gmail.com>
2020-07-10 15:16:02 +0100

    Chapter 25 - byte, short, long and width

--- ByteShortIntLong/src/com/javamasterclass/Main.java
+++ ByteShortIntLong/src/com/javamasterclass/Main.java
@@ -16,5 +16,29 @@ public class Main {
 		System.out.println("Busted MIN value = " + (myMinIntValue - 1));
 
 		int myMaxIntTest = 2_147_483_647;
+
+		byte myMinByteValue = Byte.MIN_VALUE;
+		byte myMaxByteValue = Byte.MAX_VALUE;
+
+		System.out.println("byte MIN_VALUE: " + myMinByteValue);
+		System.out.println("byte MAX_VALUE: " + myMaxByteValue);
+
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+
+		System.out.println("short MIN_VALUE: " + myMinShortValue);
+		System.out.println("short MAX_VALUE: " + myMaxShortValue);
+
+		long myLongValue = 100;
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+
+		System.out.println("long MIN_VALUE: " + myMinLongValue);
+		System.out.println("long MAX_VALUE: " + myMaxLongValue);
+		long bigLongLiteralValue = 2_147_483_647_234L;
+		System.out.println(bigLongLiteralValue);
+
+		short bigShortLiteralValue = 32767;
+
 	}
 }
