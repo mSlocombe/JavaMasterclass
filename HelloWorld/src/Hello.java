300b4e35e3208b3f27fde2093ecf2fcce50ef17d
mSlocombe <slocombe90@gmail.com>
2020-07-10 14:22:44 +0100

    Chapter 23 - Starting out with Expressions

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,12 @@ public class Hello {
 		System.out.println("Hello Matt!");
 
 		int myFirstNumber = (10 + 5) + (2 * 10);
-		System.out.println(myFirstNumber);
+		int mySecondNumber = 12;
+		int myThirdNumber = myFirstNumber * 2;
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+		System.out.println("myTotal = " + myTotal);
+
+		int myLastOne = 1000 - myTotal;
+		System.out.println("1000 - " + myTotal + " = " + myLastOne);
 	}
 }
