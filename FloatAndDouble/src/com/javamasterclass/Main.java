a38913e13a19c5832d8ffb5ddf7655d6df5d8049
mSlocombe <slocombe90@gmail.com>
2020-07-11 08:34:59 +0100

    Chapter 29 - Float And Double

--- FloatAndDouble/src/com/javamasterclass/Main.java
+++ FloatAndDouble/src/com/javamasterclass/Main.java
@@ -16,8 +16,26 @@ public class Main {
 		System.out.println("Double min: " + myMinDoubleValue);
 		System.out.println("Double max: " + myMaxDoubleValue);
 
-		int myIntValue = 5;
-		float myFloatValue = 5.25f; // without f assumes double; could also be (float)5.25 but this isn't common.
-		double myDoubleValue = 5.25d;
+		int myIntValue = 5 / 3;
+		float myFloatValue = 5f / 3f; // without f assumes double; could also be (float)5.25 but this isn't common.
+		double myDoubleValue = 5.0 / 3.0;
+
+		System.out.println("myIntValue: " + myIntValue);
+		System.out.println("myFloatValue: " + myFloatValue);
+		System.out.println("myDoubleValue: " + myDoubleValue);
+
+		// 1. Create a variable with the appropriate type to store the number of pounds to be converted to kilograms.
+		// 2. Calculate the result i.e. the number of kilograms based on the contents of the variable above and store the result in a 2nd appropriate variable.
+		// 3. Print out the result
+
+		double pounds = 200d;
+		double kilograms = pounds * 0.45359237d;
+
+		System.out.println(pounds + "lbs is " + kilograms + "kg");
+
+		double pi = Math.PI;
+		double anotherNumber = 3_000_000.4_567_890d;
+		System.out.println(pi);
+		System.out.println(anotherNumber);
     }
 }
